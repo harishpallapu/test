cefd329d58dce0a288af699965213097abfcf3df
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2025-02-14 10:46:52 -0600

    Update Application.java

--- Application.java
+++ Application.java
@@ -11,3 +11,16 @@ public class HealthAppApplication extends SpringBootServletInitializer {
         SpringApplication.run(HealthAppApplication.class, args);
     }
 }
+package com.example.healthapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String getHealthStatus() {
+        return "Health status is good!";
+    }
+}
